d07e6ceaf472e516f6f53705ab226ef42a3dd720
miyuki <t.miyuki082@gmail.com>
2021-11-28 18:14:41 +0900

    4回目

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :relationships, only: [:create, :destroy]
 
-  resources :cameras do
-    
+  resources :cameras do  
     resources :likes, only: [:create, :destroy]
     resources :comments, only: [:create, :destroy]
   end
+
+  resources :users, :only => [:index, :show]
+  
+  resources :messages, :only => [:create]
+  resources :rooms, :only => [:create, :show, :index]
 
   resources :maps
   root 'hello#index'
